a8cd669a3b9a17817123b490c8e6301542ccdab4
Kubasik <tomkubasik.pro@gmail.com>
2021-03-17 19:11:21 +0100

    Progress Breathing Bar

---
+++ Assets/Player/Scripts/BreathingProgressBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BreathingProgressBar : MonoBehaviour
+{
+    public Image image;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = ControlState.levelOfBreathing / 100;
+    }
+}
