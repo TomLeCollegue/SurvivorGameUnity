773a6bf15df8706847a77e0d92297f26846b46ae
Kubasik <tomkubasik.pro@gmail.com>
2021-03-20 09:26:48 +0100

    early temp management

---
+++ Assets/Player/Scripts/TemperatureManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureManager : MonoBehaviour
+{
+
+
+    public float temperaturePlayer = 30.0f;
+    public float backupTemp;
+
+    private void OnTriggerEnter(Collider volume)
+    {
+        if (volume.GetComponent<DataVolume>())
+        {
+            backupTemp = temperaturePlayer;
+            DataVolume dataVolume = volume.GetComponent<DataVolume>();
+            temperaturePlayer = dataVolume.temperature;
+        }
+    
+        if (volume.GetComponent<DataVolume>())
+        {
+            temperaturePlayer = backupTemp;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        DataPlayer.singleton.warm = temperaturePlayer * 4;
+    }
+}
